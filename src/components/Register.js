829a1800d98298f9a7eecf13123f05215abd1c6a
KChopde96 <kanchanchopde26@gmail.com>
2025-05-19 23:44:52 -0400

    corrections with registration

--- src/components/Register.js
+++ src/components/Register.js
@@ -12,7 +12,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      const response = await api.post('/auth/register', { username: email, password }, 
+      const response = await api.post('/auth/register', { email: email, password: password }, 
         { headers: { "Content-Type": "application/json" } }
       );
 
