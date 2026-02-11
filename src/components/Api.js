bc4c27d4dd090c82105e4bc9d7656dcd40180ecb
Kanchan Chopde <73822090+KChopde@users.noreply.github.com>
2026-02-11 18:09:17 -0500

    Attach auth token to API requests

--- src/components/Api.js
+++ src/components/Api.js
@@ -6,4 +6,17 @@ const api = axios.create({
   baseURL: backendUrl,
 });
 
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('access_token');
+
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers.Authorization = token.startsWith('Bearer ')
+      ? token
+      : `Bearer ${token}`;
+  }
+
+  return config;
+});
+
 export default api;
